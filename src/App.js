dd1aa9f59f8d73162c54490f5d4f0c5369480cae
emmanuelojiji <emmanuelojiji@hotmail.com>
2022-03-22 10:06:26 +0000

    Header max-width change

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import './App.css';
-import { useState } from 'react'
-import Card from './components/Card';
-import CreateBadgeModal from './components/CreateBadgeModal';
+import "./App.css";
+import { useState } from "react";
+import Card from "./components/Card";
+import CreateBadgeModal from "./components/CreateBadgeModal";
 
 function App() {
-
-const [isModalVisible, setIsModalVisible] = useState(false)
-
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   return (
     <div className="App">
+      {isModalVisible && <CreateBadgeModal />}
 
-  {isModalVisible && <CreateBadgeModal  />}
+      <header>
+        <button onClick={() => setIsModalVisible(true)}>Create badge</button>
+      </header>
 
-
-
-<header>
-  <button onClick={() => setIsModalVisible(true)}>Create badge</button>
-</header>
-
-
-<div className="grid-container">
-<Card badgeName='Inclusion Champ' badgeDescription='Finished ten inclusion courses'/>
-<Card badgeName='The Underdog' badgeDescription='Finished ten inclusion courses'/>
-<Card badgeName='Fast Learner' badgeDescription='Finished ten inclusion courses'/>
-<Card badgeName='Most Compassionate' badgeDescription='Finished ten inclusion courses'/>
-</div>
+      <div className="grid-container">
+        <Card
+          badgeName="Inclusion Champ"
+          badgeDescription="Finished ten inclusion courses"
+        />
+        <Card
+          badgeName="The Underdog"
+          badgeDescription="Finished ten inclusion courses"
+        />
+        <Card
+          badgeName="Fast Learner"
+          badgeDescription="Finished ten inclusion courses"
+        />
+        <Card
+          badgeName="Most Compassionate"
+          badgeDescription="Finished ten inclusion courses"
+        />
+      </div>
     </div>
   );
 }
-
 export default App;
